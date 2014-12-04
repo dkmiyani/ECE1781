cc38cc69d27375301e070933d112117f6ec6e685
s2e <s2e@debian.com.csl.deb>
2014-12-04 18:43:40 -0500

    Added 32bit debian compiled busybox 0_lib containing shared library and test prorgam bbtest.c (plus 32 bit compiled binary). Make sure to export LD_LIBRARY_PATH=<path to 0_lib>:/home/s2e/ECE1781/s2e/0_lib: before executing

---
+++ s2e/bbtest.c
@@ -0,0 +1,52 @@
+/*
+  	make menuconfig
+	(enable shared library)
+	make
+	
+	cd ~/busybox/0_lib
+	cp libbbusybox.so.1.22.1 libbbusybox.so
+	
+	cd ~/testProgram
+	gcc -o test test.c -L<path/to/0_lib> -lbusybox
+	export LD_LIBRARY_PATH=/path/to/0_lib:$LD_LIBRARY_PATH
+
+	./test
+	
+*/
+
+
+extern int lbb_main(char **argv);
+
+int main()
+{
+    char* strarray[] = {"echo", "hello world!", 0};
+    lbb_main(strarray);
+
+    return 0;
+}
+
+
+
+/*
+
+ compile this file as follows:
+	gcc -o test test.c libecho.a 
+
+ compile echo files as follows to generate static library
+	cd coreutils-8.23
+	./configure
+	make all
+	cd src
+	 gcc -std=gnu99 -I../lib -O2 -g -c -o echo.o echo.c
+	ld -r -o tmpecho.o echo.o ../lib/libcoreutils.a libver.a 
+	ar -cvq libecho.a tmpecho.o
+
+*/
+
+/*#include "echo.h"
+
+int main() {
+	char* str[] = {"unused string", "it works!"};
+	printecho(2, str);
+	return 0;
+}*/
